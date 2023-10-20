abac18ddafacdfb0bf27d1d31f750e5ab68b30c1
Jordan Paoletti <jpaoletti@clearwateranalytics.com>
2023-10-20 12:36:01 -0700

    aoc y2015 day1

--- kotlin/src/main/kotlin/com/jpaoletti/aoc/y2015/Day1.kt
+++ kotlin/src/main/kotlin/com/jpaoletti/aoc/y2015/Day1.kt
@@ -4,6 +4,42 @@ import com.jpaoletti.aoc.inputLines
 
 fun main() {
     println("Day 1")
-    val input = inputLines(year = 2015, day = 1, part = 1)
-    println("Part 1: ${input.first}")
+    pt1()
+    pt2()
+}
+
+fun pt2() {
+    println("Part 2")
+    val input = inputLines(year = 2015, day = 1, part = 1).first() // part 2 uses same input
+
+    var currFloor = 0
+    var place = 1
+    for (c in input) {
+        when (c) {
+            '(' -> currFloor++
+            ')' -> currFloor--
+        }
+
+        if (currFloor < 0) {
+            println("Enters basement at $place")
+            break
+        }
+
+        place++
+    }
+}
+
+fun pt1() {
+    println("Part 1")
+    val input = inputLines(year = 2015, day = 1, part = 1).first()
+
+    var currFloor = 0
+    input.forEach { c ->
+        when (c) {
+            '(' -> currFloor++
+            ')' -> currFloor--
+        }
+    }
+
+    println("Floor $currFloor")
 }
